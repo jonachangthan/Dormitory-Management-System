d1a8c9ba45717ebdd330321b5a83312a5035fe1f
Homie Huang <yaharry123@gmail.com>
2023-06-01 18:13:59 +0800

    timeSetup.js add a class check due

--- model/timeSetup.js
+++ model/timeSetup.js
@@ -1,18 +1,36 @@
-const applyOpeningDate = new Date('2023-06-01');
-const applyDueDate = new Date('2023-06-30');
+// you can use class like this:
 
-const semesterStartDate = new Date('2023-09-01');
-const semesterCloseDate = new Date('2024-02-01');
+// due = require('./model/timeSetup');
+// due.checkApplyDue() ? console.log('okay') : console.log('expired');
 
-const paymentDueDate = new Date('2023-09-30');
+class checkDue {
+    // 學期開始結束時間
+    static semesterStartDate = new Date('2023-09-01T00:00:00Z');
+    static semesterCloseDate = new Date('2024-02-01T23:59:59Z');
 
-var currentDate = new Date('2023-06-02'); // for testing 
+    // 住宿申請開放時間
+    static applyOpeningDate = new Date('2023-06-01T00:00:00Z');
+    static applyDueDate = new Date('2023-06-30T23:59:59Z');
 
-module.exports = {
-    applyOpeningDate,
-    applyDueDate,
-    semesterStartDate,
-    semesterCloseDate,
-    paymentDueDate,
-    currentDate
+    // 繳費deadline
+    static paymentDueDate = new Date('2023-09-30T23:59:59Z');
+
+    // 退換宿申請開放時間
+    static exitExchanegOpeningDate = new Date('2023-06-01T00:00:00Z');
+    static exitExchanegDueDate = new Date('2023-06-30T23:59:59Z');
+
+    static currentDate = new Date();
+
+    static checkApplyDue(){
+        // console.log("current date : ", checkDue.currentDate)
+        console.log("apply opening date : ", checkDue.applyOpeningDate)
+        if(checkDue.currentDate > checkDue.applyOpeningDate && checkDue.currentDate < checkDue.applyDueDate){
+            return true
+        }else{
+            return false
+        }
+    }
+
 };
+
+module.exports = checkDue;
