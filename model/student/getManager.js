782fc18f9e5f4ceb03e8117c1f901dcb798b420b
Homie Huang <yaharry123@gmail.com>
2023-06-12 01:24:48 +0800

    student getmanager.js

--- model/student/getManager.js
+++ model/student/getManager.js
@@ -2,7 +2,7 @@ const db = require("./../database")
 
 module.exports = function getManagerData() {   
     return new Promise((resolve, reject) => {
-        sql = `SELECT manager.*, account.Permission FROM manager JOIN account ON manager.M_ID = account.UserName`
+        sql = `SELECT manager.*, account.Permission FROM manager JOIN account ON manager.M_ID = account.UserName WHERE account.Permission = 1`
         db.query(sql, async (error, results) => {
             if (error) {
                 console.log(error);
