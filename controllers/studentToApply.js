1958282a8ff9609eb6ff6322173d03dc3857dd62
ChenBoAn <kevink0725n@gmail.com>
2023-06-14 00:44:08 +0800

    modify

--- controllers/studentToApply.js
+++ controllers/studentToApply.js
@@ -11,7 +11,7 @@ exports.get = (req, res) => {
             console.log(error)
             res.render('error')
         }
-        if(results.length==0){
+        if (results.length == 0) {
             return res.render('student_to_apply', {
                 notapplied: true
             });
@@ -25,44 +25,46 @@ exports.get = (req, res) => {
             results.A_Approval = "未審核"
         } else if (results.A_Approval == 1) {
             results.A_Approval = "已核可"
-        } else{
+        } else {
             results.A_Approval = "未核可"
         }
 
         results.A_Bill = results.A_Bill == 0 ? "未繳交" : "已繳交"
 
         results = [results]
-        
-        if(!approval){
+
+        if (!approval) {
             html = `<link href="/p1.css" rel="stylesheet" />
             <div class="container">
                <div class="progress-container">
                <div id="progress" class="progress"></div>
                <div class="circle active">
-                 1 
-                 <span style="color:black;font-weight:bold">已申請完畢 請等待管理員核可</span>
+                1 
+                <span style="color:black;font-weight:bold">已申請完畢 請等待管理員核可</span>  
                </div>
                <div class="circle" style="  background-color: #215199;border-color: 94b7eb;">
                  2
+                 <br>
                </div>
                <div class="circle" style="  background-color: #215199;border-color: 94b7eb;">
                  3
+                 <br>
                </div>
              </div>
            </div> 
             `
-                        
+
             return res.render('student_to_apply', {
                 message: results,
                 notapplied: false,
                 approval: approval,
-                html:html
+                html: html
             });
-            
-        }else if(results[0].A_Approval=="已核可" && results[0].A_Bill=="未繳交"){
-                getStudentDormitoryInfo(req.user.UserName).then(result => {
-                    room = result
-                    html = `<link href="/p2.css" rel="stylesheet" />
+
+        } else if (results[0].A_Approval == "已核可" && results[0].A_Bill == "未繳交") {
+            getStudentDormitoryInfo(req.user.UserName).then(result => {
+                room = result
+                html = `<link href="/p2.css" rel="stylesheet" />
                     <div class="container">
                     <div class="progress-container"> 
                         <div id="progress" class="progress"></div>
@@ -75,15 +77,15 @@ exports.get = (req, res) => {
                         <div class="circle" style=" background-color: #215199;border-color: 94b7eb;">3</div>
                     </div>
                     </div> `
-                    return res.render('student_to_apply', {
-                        message: results,
-                        notapplied: false,
-                        approval: approval,
-                        html:html
-                    });
-                })
+                return res.render('student_to_apply', {
+                    message: results,
+                    notapplied: false,
+                    approval: approval,
+                    html: html
+                });
+            })
         }
-        else{
+        else {
             getStudentDormitoryInfo(req.user.UserName).then(result => {
                 room = result
                 html = `<link href="/p3.css" rel="stylesheet" />
@@ -105,7 +107,7 @@ exports.get = (req, res) => {
                     message: results,
                     notapplied: false,
                     approval: approval,
-                    html:html
+                    html: html
                 });
             })
         }
@@ -113,32 +115,32 @@ exports.get = (req, res) => {
 }
 
 exports.apply = (req, res) => {
-    db.query('SELECT * From student Where ?',{ S_ID:req.user.UserName}, (error, results) => {
+    db.query('SELECT * From student Where ?', { S_ID: req.user.UserName }, (error, results) => {
         if (error) {
             console.log(error)
             res.render('error')
         }
-        db.query('INSERT INTO application SET ?',  
-                { A_Student_ID : results[0].S_ID, A_Date:Mdate , A_Approval:2 , A_Bill:0}, (error) => {
-            if (error) {
-                console.log(error)
-                res.render('error')
-            }
-            db.query('SELECT * From application Where ?',{A_Student_ID:req.user.UserName}, (error, results) => {
+        db.query('INSERT INTO application SET ?',
+            { A_Student_ID: results[0].S_ID, A_Date: Mdate, A_Approval: 2, A_Bill: 0 }, (error) => {
                 if (error) {
                     console.log(error)
                     res.render('error')
                 }
-                results.forEach(element => {
-                    element.A_Date = element.A_Date.getFullYear()+ '-' + (parseInt(element.A_Date.getMonth()) + 1) + '-' + element.A_Date.getDate()
-                    if(element.A_Approval==2){
-                        element.A_Approval = "未審核"
+                db.query('SELECT * From application Where ?', { A_Student_ID: req.user.UserName }, (error, results) => {
+                    if (error) {
+                        console.log(error)
+                        res.render('error')
                     }
-                    if(element.A_Bill==0){
-                        element.A_Bill = "未繳交"
-                    }
-                });
-                html = `<link href="/p1.css" rel="stylesheet" />
+                    results.forEach(element => {
+                        element.A_Date = element.A_Date.getFullYear() + '-' + (parseInt(element.A_Date.getMonth()) + 1) + '-' + element.A_Date.getDate()
+                        if (element.A_Approval == 2) {
+                            element.A_Approval = "未審核"
+                        }
+                        if (element.A_Bill == 0) {
+                            element.A_Bill = "未繳交"
+                        }
+                    });
+                    html = `<link href="/p1.css" rel="stylesheet" />
                 <div class="container">
                    <div class="progress-container">
                    <div id="progress" class="progress"></div>
@@ -154,25 +156,25 @@ exports.apply = (req, res) => {
                    </div>
                  </div>
                </div> `
-                return res.render('student_to_apply', {
-                    message:results,
-                    notapplied:false,
-                    approval:false,
-                    html:html
-                });
+                    return res.render('student_to_apply', {
+                        message: results,
+                        notapplied: false,
+                        approval: false,
+                        html: html
+                    });
+                })
             })
-        })
     })
 }
 
 exports.delete = (req, res) => {
-    db.query('DELETE FROM application Where ?',{A_Student_ID:req.user.UserName}, (error, results) => {
+    db.query('DELETE FROM application Where ?', { A_Student_ID: req.user.UserName }, (error, results) => {
         if (error) {
             console.log(error)
         }
         else {
             return res.render('student_to_apply', {
-                notapplied:true
+                notapplied: true
             })
         }
     })
@@ -188,7 +190,7 @@ exports.bill = (req, res) => {
         }
         else {
             return res.render('bill', {
-                student : results[0]
+                student: results[0]
             })
         }
     })
